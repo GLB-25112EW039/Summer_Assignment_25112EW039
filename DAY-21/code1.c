f31d7947bca4640565a3c3b3eff86e74dd27bec2
nishainfinity0-cmd <nishainfinity0@gmail.com>
2026-06-21 09:41:18 +0530

    Finished day-21

---
+++ DAY-21/code1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    scanf("%s", str);
+
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    printf("%d\n", length);
+    return 0;
+}
